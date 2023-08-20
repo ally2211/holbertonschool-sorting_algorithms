96232935861719adcdf36e3172200149d65314e4
root <6677@holbertonstudents.com>
2023-08-20 14:03:13 -0700

    bubble NULL

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,5 +1,10 @@
 #include "sort.h"
 
+/**
+ * swap - Function to swap two integer values.
+ * @xp: Pointer to the first integer.
+ * @yp: Pointer to the second integer.
+ */
 void swap(int *xp, int *yp)
 {
 	int temp = *xp;
@@ -7,9 +12,20 @@ void swap(int *xp, int *yp)
 	*yp = temp;
 }
 
+/**
+ * bubble_sort - Function to sort an array using the Bubble Sort algorithm.
+ * It also prints the array each time two elements are swapped.
+ * @array: Pointer to the array of integers.
+ * @size: Size of the array.
+ */
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
+
+	if (array == NULL || size == 0)
+	{
+		return;
+	}
 
 	for (i = 0; i < size - 1; i++)
 	{
