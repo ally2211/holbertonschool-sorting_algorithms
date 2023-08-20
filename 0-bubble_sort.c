2b331e918cad2ba58eb5191566dcfe189ba912dc
root <6677@holbertonstudents.com>
2023-08-20 13:49:15 -0700

    bubble

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,9 +9,11 @@ void swap(int *xp, int *yp)
 
 void bubble_sort(int *array, size_t size)
 {
-	for (size_t i = 0; i < size - 1; i++)
+	size_t i, j;
+
+	for (i = 0; i < size - 1; i++)
 	{
-		for (size_t j = 0; j < size - i - 1; j++)
+		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
